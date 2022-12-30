10c07eee02494ee6db5925e499278996537b0267
postman77 <98200639+postman77@users.noreply.github.com>
2022-12-29 23:04:05 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,173 @@
+let timeLeft = document.querySelector(".time-remain");
+let quizContainer = document.getElementById("container");
+let nextBtn = document.getElementById("next-btn");
+let countOfQuestion = document.querySelector(".question");
+let displayContainer = document.getElementById("display-container");
+let scoreContainer = document.querySelector(".score-container");
+let restart = document.getElementById("restart");
+let userScore = document.getElementById("user-score");
+let startScreen = document.querySelector(".start-screen");
+let startButton = document.getElementById("start-btn");
+let questionCount;
+let scoreCount = 0;
+let count = 11;
+let countdown;
+
+const quizArray = [
+  {
+    id: "0",
+    question: "What NBA player scored 100 points on March 2, 1962?",
+    options: [
+        "Elgin Baylor",
+        "Bill Russell",
+        "Kareem Abdul-Jabbar",
+        "Wilt Chamberlain"
+      ],
+    correct: "Wilt Chamberlain",
+  },
+  {
+    id: "1",
+    question: "Who was the first player in NBA history to be elected league MVP by a unanimous vote?",
+    options: [
+        "Lebron James",
+        "Stephen Curry",
+        "Magic Johnson",
+        "Michael Jordan"
+      ],
+    correct: "Stephen Curry",
+  },
+  {
+    id: "2",
+    question: "What team owns the longest winning streak in NBA history?",
+    options: [
+        "Golden State Warriors",
+        "Miami Heat",
+        "Los Angeles Lakers",
+        "Chicago Bulls"
+      ],
+    correct: "Los Angeles Lakers",
+  },
+  {
+    id: "3",
+    question: "Who was the youngest player to score 10,000 points in the NBA?",
+    options: [
+        "LeBron James",
+        "Kobe Bryant",
+        "Wilt Chamberlain",
+        "Michael Jordan"
+      ],
+    correct: "LeBron James",
+  },
+];
+
+restart.addEventListener("click", () => {
+  initial();
+  displayContainer.classList.remove("hide");
+  scoreContainer.classList.add("hide");
+});
+
+nextBtn.addEventListener(
+  "click",
+  (displayNext = () => {
+    questionCount += 1;
+    if (questionCount == quizArray.length) {
+      displayContainer.classList.add("hide");
+      scoreContainer.classList.remove("hide");
+      userScore.innerHTML =
+        "Your score is " + scoreCount + " out of " + questionCount;
+    } else {
+      countOfQuestion.innerHTML =
+        questionCount + 1 + " of " + quizArray.length + " Question";
+      quizDisplay(questionCount);
+      count = 11;
+      clearInterval(countdown);
+      timerDisplay();
+    }
+  })
+);
+
+const timerDisplay = () => {
+  countdown = setInterval(() => {
+    count--;
+    timeLeft.innerHTML = `${count}s`;
+    if (count == 0) {
+      clearInterval(countdown);
+      displayNext();
+    }
+  }, 1000);
+};
+
+const quizDisplay = (questionCount) => {
+  let quizCards = document.querySelectorAll(".container-mid");
+  quizCards.forEach((card) => {
+    card.classList.add("hide");
+  });
+  quizCards[questionCount].classList.remove("hide");
+};
+
+function quizCreator() {
+  quizArray.sort(() => Math.random() - 0.5);
+  for (let i of quizArray) {
+    i.options.sort(() => Math.random() - 0.5);
+    let div = document.createElement("div");
+    div.classList.add("container-mid", "hide");
+    countOfQuestion.innerHTML = 1 + " of " + quizArray.length + " Question";
+    let question_DIV = document.createElement("p");
+    question_DIV.classList.add("question");
+    question_DIV.innerHTML = i.question;
+    div.appendChild(question_DIV);
+    div.innerHTML += `
+    <button class="option-div" onclick="checker(this)">${i.options[0]}</button>
+     <button class="option-div" onclick="checker(this)">${i.options[1]}</button>
+      <button class="option-div" onclick="checker(this)">${i.options[2]}</button>
+       <button class="option-div" onclick="checker(this)">${i.options[3]}</button>
+    `;
+    quizContainer.appendChild(div);
+  }
+}
+
+function checker(userOption) {
+  let userSolution = userOption.innerText;
+  let question =
+    document.getElementsByClassName("container-mid")[questionCount];
+  let options = question.querySelectorAll(".option-div");
+
+  if (userSolution === quizArray[questionCount].correct) {
+    userOption.classList.add("correct");
+    scoreCount++;
+  } else {
+    userOption.classList.add("incorrect");
+    options.forEach((element) => {
+      if (element.innerText == quizArray[questionCount].correct) {
+        element.classList.add("correct");
+      }
+    });
+  }
+
+  clearInterval(countdown);
+  options.forEach((element) => {
+    element.disabled = true;
+  });
+}
+
+function initial() {
+  quizContainer.innerHTML = "";
+  questionCount = 0;
+  scoreCount = 0;
+  count = 11;
+  clearInterval(countdown);
+  timerDisplay();
+  quizCreator();
+  quizDisplay(questionCount);
+}
+
+startButton.addEventListener("click", () => {
+  startScreen.classList.add("hide");
+  displayContainer.classList.remove("hide");
+  initial();
+});
+
+window.onload = () => {
+  startScreen.classList.remove("hide");
+  displayContainer.classList.add("hide");
+};
